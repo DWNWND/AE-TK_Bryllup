d597b46f38e9d66c17613a3bed059796295a1888
Thea <theola51924@stud.noroff.no>
2024-02-23 14:36:34 +0700

    final touches

---
+++ index 2.js
@@ -0,0 +1,42 @@
+// //https://alvarotrigo.com/blog/css-animations-scroll/
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    // var elementVisible = 150; juster dette tallet for når det skal reveales
+    var elementVisible = -50;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+
+const sections = document.querySelectorAll(".section");
+const navLi = document.querySelectorAll(".nav-item");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 150) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
